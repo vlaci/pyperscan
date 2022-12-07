15b9791b0c67698c4fe717408f016c841800498f
László Vaskó <1771332+vlaci@users.noreply.github.com>
2022-12-07 18:06:21 +0100

    nix with glibc217

--- flake.nix
+++ flake.nix
@@ -5,9 +5,11 @@
     maturin.url = "github:PyO3/maturin";
     maturin.flake = false;
     rust-overlay.url = "github:oxalica/rust-overlay";
+    nix-old-libc.url = "git+ssh://git@git.sr.ht/~vlaci/nix-old-libc";
+    nix-old-libc.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, utils, maturin, rust-overlay }:
+  outputs = { self, nixpkgs, utils, maturin, rust-overlay, nix-old-libc }:
     utils.lib.eachDefaultSystem
       (system:
         let
@@ -44,6 +46,10 @@
                 vectorscan = drv.override { vendorVectorscan = true; };
               };
             });
+
+          stdenv = nix-old-libc.packages.${system}.stdenv-glibc217;
+          hyperscan = pkgs.hyperscan.override { inherit stdenv; };
+          python3Packages = (pkgs.python38.override { inherit stdenv; }).pkgs;
         in
         {
           packages = pkgs;
@@ -52,21 +58,25 @@
             let
               inherit (pkgs.lib) filter hasSuffix;
               noHooks = filter (drv: !(hasSuffix "hook.sh" drv.name));
-              pyperscan' = pyperscan.override { python3Packages = pkgs.python38Packages; };
+              pyperscan' = pyperscan.override { inherit python3Packages hyperscan; };
 
             in
-            with pkgs; mkShell {
+            with pkgs; (mkShell.override { inherit stdenv; }) {
               nativeBuildInputs = noHooks pyperscan'.hyperscan.nativeBuildInputs;
               buildInputs = [
+                glibc_multi
                 just
                 pre-commit
                 rustToolchainDev
                 pkgs.maturin
+                pkgs.zig
                 pkgs.rust-bin.nightly.latest.rust-analyzer
               ]
               ++ pyperscan'.hyperscan.buildInputs
               ++ (optionals (system == "x86_64-linux")
                 pyperscan'.buildInputs);
+              LOCALE_ARCHIVE = "${stdenv.cc.libc}/lib/locale/locale-archive";
+
             };
         });
 }
