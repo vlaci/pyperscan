c0d89dedf81420a67c71b77929094b30bd3d6f29
László Vaskó <1771332+vlaci@users.noreply.github.com>
2025-02-10 22:40:42 +0100

    chore(flake): reformatting with nixfmt-rfc-style

--- flake.nix
+++ flake.nix
@@ -16,42 +16,77 @@
     };
   };
 
-  outputs = { self, nixpkgs, crane, fenix, advisory-db, ... }@inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      crane,
+      fenix,
+      advisory-db,
+      ...
+    }@inputs:
     let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs {
-        inherit system;
-        overlays = [
-          self.overlays.default
-          fenix.overlays.default
-        ];
-      });
+      nixpkgsFor = forAllSystems (
+        system:
+        import nixpkgs {
+          inherit system;
+          overlays = [
+            self.overlays.default
+            fenix.overlays.default
+          ];
+        }
+      );
     in
     {
-      overlays.default = final: prev:
+      overlays.default =
+        final: prev:
         let
-          stdenv = if prev.stdenv.isDarwin then final.overrideLibcxx final.darwin.apple_sdk_11_0.llvmPackages_14.stdenv else prev.stdenv;
-          rustPlatform = final.makeRustPlatform { inherit stdenv; inherit (final) rustc cargo; };
+          stdenv =
+            if prev.stdenv.isDarwin then
+              final.overrideLibcxx final.darwin.apple_sdk_11_0.llvmPackages_14.stdenv
+            else
+              prev.stdenv;
+          rustPlatform = final.makeRustPlatform {
+            inherit stdenv;
+            inherit (final) rustc cargo;
+          };
         in
         {
           pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
             (python-final: python-prev: {
-              pyperscan = final.callPackage (import ./pyperscan.nix inputs) { inherit stdenv rustPlatform; python3 = python-final.python; };
+              pyperscan = final.callPackage (import ./pyperscan.nix inputs) {
+                inherit stdenv rustPlatform;
+                python3 = python-final.python;
+              };
             })
           ];
           vectorscan = prev.vectorscan.override { inherit stdenv; };
         };
-      checks = forAllSystems (system:
+      checks = forAllSystems (
+        system:
         let
           inherit (nixpkgsFor.${system}) rustPlatform;
           inherit (nixpkgsFor.${system}.python3Packages) pyperscan;
           nativeBuildInputs = with rustPlatform; [
             bindgenHook
           ];
-          inherit (pyperscan) cargoArtifacts craneLib commonArgs libpyperscan src;
+          inherit (pyperscan)
+            cargoArtifacts
+            craneLib
+            commonArgs
+            libpyperscan
+            src
+            ;
         in
-        pyperscan.passthru.tests // {
+        pyperscan.passthru.tests
+        // {
           # Build the crate as part of `nix flake check` for convenience
           inherit libpyperscan;
 
@@ -61,14 +96,20 @@
           # Note that this is done as a separate derivation so that
           # we can block the CI if there are issues here, but not
           # prevent downstream consumers from building our crate by itself.
-          libpyperscan-clippy = craneLib.cargoClippy (commonArgs // {
-            inherit nativeBuildInputs cargoArtifacts;
-            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-          });
+          libpyperscan-clippy = craneLib.cargoClippy (
+            commonArgs
+            // {
+              inherit nativeBuildInputs cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            }
+          );
 
-          libpyperscan-doc = craneLib.cargoDoc (commonArgs // {
-            inherit nativeBuildInputs cargoArtifacts;
-          });
+          libpyperscan-doc = craneLib.cargoDoc (
+            commonArgs
+            // {
+              inherit nativeBuildInputs cargoArtifacts;
+            }
+          );
 
           # Check formatting
           libpyperscan-fmt = craneLib.cargoFmt {
@@ -84,42 +125,48 @@
 
       formatter = forAllSystems (system: nixpkgsFor.${system}.nixpkgs-fmt);
 
-      packages = forAllSystems (system:
+      packages = forAllSystems (
+        system:
         let
           inherit (nixpkgsFor.${system}.python3Packages) pyperscan;
         in
         {
           inherit pyperscan;
           default = pyperscan;
-        });
+        }
+      );
 
-      devShells = forAllSystems (system:
+      devShells = forAllSystems (
+        system:
         let
           pkgs = nixpkgsFor.${system};
         in
         {
-          default = with pkgs; mkShell {
-            buildInputs = [
-              just
-              maturin
-              pdm
-              podman
-              pre-commit
-              boost
-              cmake
-              ragel
-              rustPlatform.bindgenHook
-              hyperscan
-              (pkgs.fenix.complete.withComponents [
-                "cargo"
-                "clippy"
-                "rust-src"
-                "rustc"
-                "rustfmt"
-              ])
-              rust-analyzer-nightly
-            ];
-          };
-        });
+          default =
+            with pkgs;
+            mkShell {
+              buildInputs = [
+                just
+                maturin
+                pdm
+                podman
+                pre-commit
+                boost
+                cmake
+                ragel
+                rustPlatform.bindgenHook
+                hyperscan
+                (pkgs.fenix.complete.withComponents [
+                  "cargo"
+                  "clippy"
+                  "rust-src"
+                  "rustc"
+                  "rustfmt"
+                ])
+                rust-analyzer-nightly
+              ];
+            };
+        }
+      );
     };
 }
