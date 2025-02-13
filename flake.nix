065cb0a5ea3349a6b22c86a06dd9b3501c6d5e17
László Vaskó <1771332+vlaci@users.noreply.github.com>
2025-02-13 23:45:30 +0100

    chore: move packaging on crane-maturin

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     crane.url = "github:ipetkov/crane";
+    crane-maturin.url = "github:vlaci/crane-maturin";
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -20,10 +21,11 @@
       self,
       nixpkgs,
       crane,
+      crane-maturin,
       rust-overlay,
       advisory-db,
       ...
-    }@inputs:
+    }:
     let
       supportedSystems = [
         "x86_64-linux"
@@ -52,18 +54,70 @@
               final.overrideLibcxx final.darwin.apple_sdk_11_0.llvmPackages_14.stdenv
             else
               prev.stdenv;
-          rustPlatform = final.makeRustPlatform {
-            inherit stdenv;
-            inherit (final) rustc cargo;
-          };
         in
         {
           pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
             (python-final: python-prev: {
-              pyperscan = final.callPackage (import ./pyperscan.nix inputs) {
-                inherit stdenv rustPlatform;
-                python3 = python-final.python;
-              };
+              pyperscan = final.callPackage (
+                {
+                  lib,
+                  system,
+                  rustPlatform,
+                  boost,
+                  hyperscan,
+                  vectorscan,
+                  vendorHyperscan ? false,
+                  vendorVectorscan ? false,
+                }:
+
+                assert vendorHyperscan -> !vendorVectorscan;
+                assert vendorVectorscan -> !vendorHyperscan;
+
+                let
+                  inherit (lib) optional optionalString;
+                  vendor = vendorHyperscan || vendorVectorscan;
+                  cmLib = crane-maturin.mkLib crane final;
+
+                  cppFilter =
+                    path: _type: builtins.match ".*/hyperscan-sys/(wrapper.h|hyperscan|vectorscan).*$" path != null;
+
+                  pyFilter =
+                    path: _type:
+                    builtins.match ".*pyi?$|.*/py.typed$|.*/pyproject.toml|.*/README.md$|.*/LICENSE" path != null;
+                  testFilter = p: t: builtins.match ".*/(tests|tests/.*\.py|examples|examples/.*\.py)$" p != null;
+                  sourceFilter = path: type: (cppFilter path type) || (cmLib.filterCargoSources path type);
+                  drv = cmLib.buildMaturinPackage {
+                    pname =
+                      "pyperscan"
+                      + optionalString vendorHyperscan "-hyperscan"
+                      + optionalString vendorVectorscan "-vectorscan";
+                    src = lib.cleanSourceWith {
+                      src = cmLib.path ./.;
+                      filter = p: t: (pyFilter p t) || (sourceFilter p t);
+                    };
+                    testSrc = lib.cleanSourceWith {
+                      src = ./.;
+                      filter = p: t: (sourceFilter p t) || (testFilter p t);
+                    };
+                    inherit advisory-db;
+
+                    nativeBuildInputs = with rustPlatform; [
+                      bindgenHook
+                    ];
+                    buildInputs =
+                      optional vendor boost
+                      ++ optional (system == "x86_64-linux") hyperscan
+                      ++ optional (system != "x86_64-linux") vectorscan;
+
+                    passthru = {
+                      shared = drv;
+                      hyperscan = drv.override { vendorHyperscan = true; };
+                      vectorscan = drv.override { vendorVectorscan = true; };
+                    };
+                  };
+                in
+                drv
+              ) { };
             })
           ];
           vectorscan = prev.vectorscan.override { inherit stdenv; };
@@ -71,55 +125,12 @@
       checks = forAllSystems (
         system:
         let
-          inherit (nixpkgsFor.${system}) rustPlatform;
           inherit (nixpkgsFor.${system}.python3Packages) pyperscan;
-          nativeBuildInputs = with rustPlatform; [
-            bindgenHook
-          ];
-          inherit (pyperscan)
-            cargoArtifacts
-            craneLib
-            commonArgs
-            libpyperscan
-            src
-            ;
         in
-        pyperscan.passthru.tests
-        // {
-          # Build the crate as part of `nix flake check` for convenience
-          inherit libpyperscan;
-
-          # Run clippy (and deny all warnings) on the crate source,
-          # again, resuing the dependency artifacts from above.
-          #
-          # Note that this is done as a separate derivation so that
-          # we can block the CI if there are issues here, but not
-          # prevent downstream consumers from building our crate by itself.
-          libpyperscan-clippy = craneLib.cargoClippy (
-            commonArgs
-            // {
-              inherit nativeBuildInputs cargoArtifacts;
-              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
-            }
-          );
-
-          libpyperscan-doc = craneLib.cargoDoc (
-            commonArgs
-            // {
-              inherit nativeBuildInputs cargoArtifacts;
-            }
-          );
-
-          # Check formatting
-          libpyperscan-fmt = craneLib.cargoFmt {
-            inherit src;
-          };
-
-          # Audit dependencies
-          libpyperscan-audit = craneLib.cargoAudit {
-            inherit src advisory-db;
-          };
-        }
+        builtins.removeAttrs pyperscan.passthru.tests [
+          "test"
+          "test-coverage"
+        ]
       );
 
       formatter = forAllSystems (system: nixpkgsFor.${system}.nixpkgs-fmt);
@@ -150,6 +161,7 @@
                 pdm
                 podman
                 pre-commit
+                openssl
                 boost
                 cmake
                 ragel
@@ -161,6 +173,7 @@
                     extensions = [
                       "cargo"
                       "clippy"
+                      "miri"
                       "rust-src"
                       "rustc"
                       "rustfmt"
